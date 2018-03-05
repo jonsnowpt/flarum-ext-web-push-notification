11c3d0c5b4869544b957fc00adafb8dfd9d4028a
DogSports <DogSports@users.noreply.github.com>
2018-03-05 22:41:31 +0100

    Update 2016_10_19_040234_add_one_signal_user_id_to_user_table.php

--- migrations/2016_10_19_040234_add_one_signal_user_id_to_user_table.php
+++ migrations/2016_10_19_040234_add_one_signal_user_id_to_user_table.php
@@ -2,14 +2,14 @@
 use Illuminate\Database\ConnectionInterface;
 use Illuminate\Database\Schema\Builder;
 return [ 
-		'up' => function (Builder $schema, ConnectionInterface $db) {
-			$schema->table ( 'users', function ($table) {
-				$table->string ( 'one_signal_user_id' );
-			} );
-		},
-		'down' => function (Builder $schema) {
-			$schema->table ( 'users', function ($table) {
-                   $table->dropColumn('one_signal_user_id');
-               });
-    }
+	'up' => function (Builder $schema, ConnectionInterface $db) {
+		$schema->table('users', function ($table) {
+			$table->string('one_signal_user_id');
+		});
+	},
+	'down' => function (Builder $schema) {
+		$schema->table('users', function ($table) {
+			$table->dropColumn('one_signal_user_id');
+		});
+	}
 ];
