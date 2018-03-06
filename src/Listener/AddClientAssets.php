e89961e34aaf75d080e05773c16ef1d0396165fc
DogSports <DogSports@users.noreply.github.com>
2018-03-06 01:05:59 +0100

    Update AddClientAssets.php

--- src/Listener/AddClientAssets.php
+++ src/Listener/AddClientAssets.php
@@ -6,27 +6,18 @@ use Flarum\Event\ConfigureWebApp;
 use Illuminate\Contracts\Events\Dispatcher;
 
 class AddClientAssets {
-	public function subscribe(Dispatcher $events) {
-		$events->listen ( ConfigureWebApp::class, [ 
-				$this,
-				'addAssets' 
-		] );
+	public function subscribe(Dispatcher $event) {
+		$events->listen(ConfigureWebApp::class, [$this, 'addAssets']);
 	}
 	public function addAssets(ConfigureWebApp $event) {
-		if ($event->isForum ()) {
-			$event->addAssets ( [ 
-					'https://cdn.onesignal.com/sdks/OneSignalSDK.js' 
-			] );
-			$event->addAssets ( [ 
-					__DIR__ . '/../../js/forum/dist/extension.js' 
-			] );
-			$event->addBootstrapper ( 'dogsports/web/push/main' );
+		if ($event->isForum()) {
+			$event->addAssets(['https://cdn.onesignal.com/sdks/OneSignalSDK.js']);
+			$event->addAssets([__DIR__.'/../../js/forum/dist/extension.js']);
+			$event->addBootstrapper('dogsports/web/push/main');
 		}
-		if ($event->isAdmin ()) {
-			$event->addAssets ( [ 
-					__DIR__ . '/../../js/admin/dist/extension.js' 
-			] );
-			$event->addBootstrapper ( 'dogsports/web/push/main' );
+		if ($event->isAdmin()) {
+			$event->addAssets([__DIR__.'/../../js/admin/dist/extension.js']);
+			$event->addBootstrapper('dogsports/web/push/main');
 		}
 	}
 }
