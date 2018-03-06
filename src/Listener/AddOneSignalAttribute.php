84d344ba54b5113b274668f4fd4a0e1505d553fa
DogSports <DogSports@users.noreply.github.com>
2018-03-06 01:11:24 +0100

    Update AddOneSignalAttribute.php

--- src/Listener/AddOneSignalAttribute.php
+++ src/Listener/AddOneSignalAttribute.php
@@ -13,16 +13,13 @@ class AddOneSignalAttribute {
 		$this->settings = $settings;
 	}
 	public function subscribe(Dispatcher $events) {
-		$events->listen ( PrepareApiAttributes::class, [ 
-				$this,
-				'addAttributes' 
-		] );
+		$events->listen(PrepareApiAttributes::class, [$this, 'addAttributes']);
 	}
 	public function addAttributes(PrepareApiAttributes $event) {
-		$event->attributes ['dogsports_web_push_app_id'] = $this->settings->get ( 'dogsports-web-push.one_signal_app_id' );
-		$event->attributes ['dogsports_web_push_subdomain'] = $this->settings->get ( 'dogsports-web-push.onesignal_subdomain' );
-		if ($event->isSerializer ( UserSerializer::class )) {
-			$event->attributes ['one_signal_user_id'] = $event->model->one_signal_user_id;
+		$event->attributes['dogsports_web_push_app_id'] = $this->settings->get('dogsports-web-push.one_signal_app_id');
+		$event->attributes['dogsports_web_push_subdomain'] = $this->settings->get('dogsports-web-push.onesignal_subdomain');
+		if ($event->isSerializer(UserSerializer::class)) {
+			$event->attributes['one_signal_user_id'] = $event->model->one_signal_user_id;
 		}
 	}
 }
