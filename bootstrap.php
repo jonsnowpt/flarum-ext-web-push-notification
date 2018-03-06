1673412038c8f46fc35bd3b5ddce89c3c08be760
DogSports <DogSports@users.noreply.github.com>
2018-03-06 01:19:35 +0100

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -6,7 +6,7 @@ use Illuminate\Contracts\Events\Dispatcher;
 use DogSports\Web\Push\Listener\AddClientAssets;
 use DogSports\Web\Push\Listener\AddHeadData;
 use DogSports\Web\Push\Listener\AddManifestRoute;
-use DogSports\Web\Push\Listener\AddOneSignalAttribute;
+use DogSports\Web\Push\Listener\AddWebPushAttribute;
 use DogSports\Web\Push\Listener\SendWebPushNotification;
 use DogSports\Web\Push\Listener\WhileUserUpdate;
 
@@ -14,7 +14,7 @@ return function (Dispatcher $events) {
     $events->subscribe(AddClientAssets::class);
     $events->subscribe(AddManifestRoute::class);
     $events->subscribe(AddHeadData::class);
-    $events->subscribe(AddOneSignalAttribute::class);
+    $events->subscribe(AddWebPushAttribute::class);
     $events->subscribe(WhileUserUpdate::class);
     $events->subscribe(SendWebPushNotification::class);
 };
