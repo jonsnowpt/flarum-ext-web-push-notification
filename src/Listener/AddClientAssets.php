b4e72e0de74ea6ac046d624edc9816addb59b09e
DogSports <DogSports@users.noreply.github.com>
2018-03-06 02:33:39 +0100

    Update AddClientAssets.php

--- src/Listener/AddClientAssets.php
+++ src/Listener/AddClientAssets.php
@@ -6,7 +6,7 @@ use Flarum\Event\ConfigureWebApp;
 use Illuminate\Contracts\Events\Dispatcher;
 
 class AddClientAssets {
-	public function subscribe(Dispatcher $event) {
+	public function subscribe(Dispatcher $events) {
 		$events->listen(ConfigureWebApp::class, [$this, 'addAssets']);
 	}
 	public function addAssets(ConfigureWebApp $event) {
